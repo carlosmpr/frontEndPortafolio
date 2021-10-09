d35267ac0bab2b02a9f2d6982c16b5b50c630962
carlosmpr <carlosmpr0508@gmail.com>
2021-10-09 12:29:20 -0400

    Sidebar created

---
+++ menudashboard/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
+
+
+function App() {
+  return (
+    <div className="flex w-screen h-screen ">
+      <Sidebar />
+     
+    </div>
+  );
+}
+
+export default App;
