bd4f3e37132806a44b2b849d24de564793680061
carlosmpr <carlosmpr0508@gmail.com>
2021-10-10 09:00:46 -0400

    added setting pages

---
+++ menudashboard/src/components/Pages/Settings.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Button from '../Buttons/Button'
+import OutlineButton from '../Buttons/OutlineButton'
+import OutlineIconButton from '../Buttons/OutlineIconButton'
+import AddCard from '../Cards/AddCard'
+import EditCard from '../Cards/EditCard'
+import IconItem from '../Items/IconItems'
+import Navigation from '../TextNavigation/Navigation'
+export default function Settings() {
+    return (
+        <div className="w-full h-full flex flex-col space-y-5">
+        <h1 className="text-2xl font-bold text-primary">Settings</h1>
+        <div className="flex w-full h-full gap-x-4">
+          <div className="flex flex-col w-72 bg-primary h-full gap-y-10 rounded-md shadow-md overflow-y-scroll p-4">
+            <IconItem
+              text="Appereance"
+              desc="Dark and Light mode. Font size"
+              icon="fas fa-heart"
+            />
+            <IconItem
+              text="Your Restaurant"
+              desc="Dark and Light mode. Font size"
+              icon="fas fa-home"
+            />
+            <IconItem
+              text="Product Management"
+              desc="Manage your Products"
+              icon="fas fa-life-ring"
+            />
+            <IconItem
+              text="Notification"
+              desc="Customize your notification"
+              icon="fas fa-bell"
+            />
+
+            <IconItem
+              text="Security"
+              desc="Configure Passworn. Pin"
+              icon="fas fa-lock"
+            />
+            <IconItem
+              text="About Us"
+              desc="Fin more info"
+              icon="fas fa-info-circle"
+            />
+          </div>
+
+          <div className="flex flex-col flex-1 bg-primary h-full gap-y-10 rounded-md shadow-md  p-4">
+            <div className="w-full flex items-center justify-between">
+              <h3 className="text-xl text-primary"> Products Management</h3>
+              <OutlineIconButton text="filter" icon="fas fa-sort" />
+            </div>
+            <Navigation />
+            <div className="w-full h-4/6 flex flex-wrap gap-x-12 gap-y-6 overflow-y-scroll">
+              <AddCard />
+              <EditCard />
+              <EditCard />
+              <EditCard />
+              <EditCard />
+              <EditCard />
+            </div>
+            <div className="flex items-center justify-evenly">
+              <OutlineButton text="Discar Changes" />
+              <Button text="Save Changes" />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
