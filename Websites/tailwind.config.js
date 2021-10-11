7fbc0d286c5a1a9558999455be7b51e730478dcf
carlosmpr <carlosmpr0508@gmail.com>
2021-10-11 15:55:05 -0400

    Website mockup

---
+++ Websites/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+       'primary': '#7C76BB',
+      'secondary': 'white',
+       'theme': '#EA7C69',
+     }),
+
+     textColor: {
+      'theme': '#503E9D',
+      'primary':'#7C76BB',
+      'secondary':'#FF87B0',
+      'white':'white',
+      'purple':'rgba(124, 58, 237)',
+      'green':'rgba(5, 150, 105)'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
